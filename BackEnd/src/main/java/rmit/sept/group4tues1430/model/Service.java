9847d1cef69d6c5cee7504f8ebe5c3d9e161e1a5
[Su-Kuen Koh 3769533] <s3769533@student.rmit.edu.au>
2020-08-21 13:15:33 +1000

    Add Service class in model

---
+++ BackEnd/src/main/java/rmit/sept/group4tues1430/model/Service.java
@@ -0,0 +1,52 @@
+package rmit.sept.group4tues1430.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //@NotBlank(message = "Service name must not be blank")
+    private String name;
+
+    //private List<Worker> serviceWorkers; //this doesn't work?
+
+    public Service() {
+
+    }
+
+    public Service(String name, Long id) {
+        this.name = name;
+        this.id = id;
+        //serviceWorkers = new ArrayList<Worker>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public void addWorker(Worker worker) {
+//        serviceWorkers.add(worker);
+//    }
+}
